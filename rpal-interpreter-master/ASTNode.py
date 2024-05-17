1b97c0b75d117caf70e948399b59c7a971eae700
hananazir-2001 <129369824+hananazir-2001@users.noreply.github.com>
2024-05-17 17:56:24 +0530

    Update ASTNode.py

--- rpal-interpreter-master/ASTNode.py
+++ rpal-interpreter-master/ASTNode.py
@@ -3,11 +3,11 @@ class ASTNode:
 
 
     def standardize(self, root):
-
+        # If the root is None, return None
         if root == None:
             return None
 
-
+        # Recursively standardize the child and sibling nodes
         root.child = self.standardize(root.child)
 
         if root.sib != None:
@@ -17,8 +17,9 @@ class ASTNode:
 
         # prev_sib = root.prev
         # next_sib = root.sib
-
+        # Using pattern matching to handle different node types
         match root.type:
+            # Handle the "let" node type
             case "let":
                 if root.child.type == "=":
                     equal = root.child
@@ -41,7 +42,7 @@ class ASTNode:
                     root.sib = next_sib
 
                     return root
-
+            # Handle the "where" node type
             case "where":
                 if root.child.sib.type== "=":
                     P = root.child
@@ -67,8 +68,9 @@ class ASTNode:
                     root.sib = next_sib
 
                     return root
-
+            # Handle the "function_form" node type
             case "function_form":
+           
                 P = root.child
                 V = P.sib
                 Vs = V.sib
@@ -79,7 +81,8 @@ class ASTNode:
                 lambda_Node = ASTNode("lambda")
                 P.sib = lambda_Node
                 lambda_Node.prev = P
-
+                
+                # Create a chain of lambda nodes for multiple arguments
                 while Vs.sib != None:
                     lambda_Node.child = V
                     lambda_Node = ASTNode("lambda")
@@ -97,7 +100,7 @@ class ASTNode:
                 return newRoot
 
             
-
+            # Handle the "within" node type
             case "within":
                 if root.child.type =="=" and root.child.sib.type == "=":
                     eq1 = root.child
@@ -128,7 +131,7 @@ class ASTNode:
                     root.sib = next_sib
 
                     return root
-
+            # Handle the "and" node type
             case "and":
                 eq = root.child
 
@@ -161,7 +164,7 @@ class ASTNode:
 
 
                 return newRoot
-
+            # Handle the "rec" node type
             case "rec":
                 eq = root.child
                 X = eq.child
@@ -189,7 +192,7 @@ class ASTNode:
 
 
                 return new_root
-
+            # Handle the "@" node type
             case "@":
                 E1 = root.child
                 N = E1.sib
@@ -208,37 +211,39 @@ class ASTNode:
                 new_root.sib=next_sib
 
                 return new_root
-
+            # For other node types, return the root as it is
             case _:
                 return root
 
         return root
 
     def __init__(self, type):
+        # Initialize the ASTNode with the given type
         self.type = type
         self.val = None
         self.source_LineNum = -1
         self.child = None
-        self.sib = None      #sibling
-        self.prev = None     #previous
-        self.indentation = 0
+        self.sib = None      #sibling node
+        self.prev = None     #previous node
+        self.indentation = 0  # used for printing the tree structure
 
     def Tree(self):
-
+        # Recursive function to traverse the tree
         if self.child:
             self.child.Tree()
         if self.sib:
             self.sib.Tree()
 
     def Tree_to_cmd(self):
-
+        # Function to print the tree structure to the command line
         for i in range(self.indentation):
             print(".", end="")
+        # Print the node type and value if available
         if self.val is not None:
             print("<"+str(self.type.split(".")[1]) +":" + str(self.val)+">")
         else:print(str(self.type))
         
-
+        # Recursively print the child and sibling nodes with increased indentation
         if self.child:
             self.child.indentation = self.indentation + 1
             self.child.Tree_to_cmd()
@@ -246,20 +251,21 @@ class ASTNode:
             self.sib.indentation = self.indentation
             self.sib.Tree_to_cmd()
 
-    # output to the file
+    
+    # Function to output the tree structure to a file
     def Tree_to_file(self, file):
 
         for i in range(self.indentation):
             file.write(".")
-        # if(self.type ==)
+        # Write the node type and value if available to the file
         if self.val is not None:
 
             file.write("<"+str(self.type.split(".")[1])+":"+str(self.val)+">" + "\n")
         else :
             file.write(str(self.type) + "\n")
-
+            
+        # Recursively write the child and sibling nodes to the file with increased indentation
         if self.child:
-
             self.child.indentation = self.indentation + 1
             self.child.Tree_to_file(file)
         if self.sib:
@@ -267,6 +273,7 @@ class ASTNode:
             self.sib.Tree_to_file(file)
 
     def createCopy (self):
+        # Function to create a copy of the current node
         node = ASTNode(self.type)
         node.val = self.val
         node.source_LineNum = self.source_LineNum
