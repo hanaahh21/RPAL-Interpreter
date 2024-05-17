13d1ba6a138cae5b02ccbad7179a938a9daf1624
hananazir-2001 <129369824+hananazir-2001@users.noreply.github.com>
2024-05-17 18:51:19 +0530

    Update Tokenizer.py

--- rpal-interpreter-master/Tokenizer.py
+++ rpal-interpreter-master/Tokenizer.py
@@ -8,13 +8,13 @@ RESERVED_KEYWORDS = ['fn','where', 'let', 'aug', 'within' ,'in' ,'rec' ,'eq','gr
 
 # --classes--
 
-# define token class
+# define token class to represent tokens in the input
 class Token():
     def __init__(self, type, value):
         self.type = type
         self.value = value
 
-# define token types enumeration
+# define token types enumeration using Enum for better type management
 class TokenType(Enum):
     RESERVED_KEYWORD = 'RESERVED_KEYWORD'
 
@@ -57,20 +57,21 @@ class TokenType(Enum):
     EOF = 'EOF'
 
 
-# Define lexical state class
+# Define lexical state class to keep track of the lexical analysis state
 class LexicalState:
     def __init__(self):
-        self.line_num = 0
-        self.curr_char = None
-        self.col_num = None
+        self.line_num = 0  # Current line number
+        self.curr_char = None  # Current character
+        self.col_num = None   # Current column number
 
-# Tokenizer class for tokenizing input text
+# Define the Tokenize class to handle the tokenization of input text
 class Tokenize:
     def __init__(self, text):
-        self.text = text
-        self.pos = 0
-        self.state = LexicalState()
+        self.text = text  # Input text to be tokenized
+        self.pos = 0   # Current position in the input text
+        self.state = LexicalState()   # Initialize lexical state
 
+        # Set initial state values
         self.state.curr_char = self.text[self.pos]
         self.state.line_num = 1
         self.state.col_num = 1
@@ -80,7 +81,7 @@ class Tokenize:
     def ErrorHandler(self):
         raise Exception('Error : parsing input')
 
-    # Move to the next character
+    # Move to the next character in the input text
     def advance(self):
         self.pos += 1
         if self.pos > len(self.text) - 1:
@@ -89,7 +90,7 @@ class Tokenize:
             self.state.curr_char = self.text[self.pos]
             self.state.col_num += 1
 
-    # Skip space characters
+    # Skip space characters in the input text
     def skip_space(self):
         while self.state.curr_char is not None and self.state.curr_char.isspace():
             if self.state.curr_char == '\n':
@@ -97,54 +98,55 @@ class Tokenize:
                 self.state.col_num = 0
             self.advance()
             
-    # Tokenize integera
+    # Tokenize integer values
     def integer(self):
         result = ''
         while self.state.curr_char is not None :
             if self.state.curr_char.isdigit():
                 result += self.state.curr_char
-                self.advance()
+                self.advance()  # Move to the next character
             elif self.state.curr_char.isalpha():
-                self.error()
+                self.error()  # Raise an error if an alphabetic character is found in an integer
             else: break
 
 
-        return int(result)
+        return int(result)   # Return the integer value
     
     
-    # Tokenize identifiers
+    # Tokenize identifiers (variable names, function names, etc.)
     def identifier(self):
         result = ''
         while self.state.curr_char is not None and (
                 self.state.curr_char.isalpha() or self.state.curr_char.isdigit() or self.state.curr_char == '_'):
             result += self.state.curr_char
-            self.advance()
-        return result
+            self.advance()   # Move to the next character
+        return result  # Return the identifier
     
 #   Tokenize comments
     def comment(self):
         result = ''
         while self.state.curr_char is not None and self.state.curr_char != '\n':
             result += self.state.curr_char
-            self.advance()
-        return result
+            self.advance()  # Move to the next character
+        return result  # Return the comment text
     
     # Check if the current character is a comment
     def isComment(self):
         if self.state.curr_char is not None and self.state.curr_char == '/':
-            return True
+            return True  # It is a comment
         else:
-            return False
+            return False  # It is not a comment
 
     # Tokenize strings
     def string(self):
         result = ''
         while self.state.curr_char is not None and self.state.curr_char != "'":
             result += self.state.curr_char
-            self.advance()
-        self.advance()
-        return result
-
+            self.advance()    # Move to the next character 
+        self.advance()    # Move past the closing quote
+        return result  # Return the string content
+        
+    # Get the next token from the input
     def getNextToken(self):
         while self.state.curr_char is not None:
 
@@ -165,8 +167,8 @@ class Tokenize:
             # handle comments
             elif self.state.curr_char == '/':
                 if self.text[self.pos + 1] == '/':
-                    self.advance()
-                    self.advance()
+                    self.advance()  # Skip the first '/'
+                    self.advance()  # Skip the second '/'
                     return Token(TokenType.COMMENT, self.comment())
 
                 else :
@@ -175,19 +177,19 @@ class Tokenize:
 
 
 
-            # tokenize string starting with '
+            # tokenize string starting with a single quote - '
             elif self.state.curr_char == "'":
-                self.advance()
+                self.advance()  # Skip the opening quote
                 return Token(TokenType.STRING, self.string())
 
 
-            # tokenize punctuation
+            # tokenize punctuation characters
             elif self.state.curr_char in PUNCTION:
                 token = Token(TokenType.PUNCTION, self.state.curr_char)
-                self.advance()
+                self.advance()  # Move to the next character
                 return token
 
-            # tokenize all single character tokens
+            # Tokenize single character tokens
             elif self.state.curr_char == '+':
                 self.advance()
                 return Token(TokenType.PLUS, '+')
@@ -284,20 +286,20 @@ class Tokenize:
                 self.advance()
                 return Token(TokenType.OR_OPERATOR, '|')
 
-            self.error()
+            self.error()   # Raise an error if no valid token is found
 
-        # return EOF at the end of file
+        # Return EOF token at the end of the input
         return Token(TokenType.EOF, None)
 
 
 # define screener class
 class Screener:
-    # initialize screeener object with tokens
+    # Initialize the Screener object with a list of tokens
     def __init__(self,tokens):
         self.text = None
         self.tokens=tokens
 
-     # merge consecutive tokens with specific patterns
+     # Merge consecutive tokens based on specific patterns
     def mergeTok(self ):
         tokens=self.tokens
 
@@ -306,56 +308,61 @@ class Screener:
             if i < len(tokens) and tokens[i].type == TokenType.MINUS and tokens[i + 1].type == TokenType.LESSER_THAN:
                 tokens[i].value = '->'
                 tokens[i].type = TokenType.TERNARY_OPERATOR
-                tokens.pop(i + 1)
+                tokens.pop(i + 1)  # Remove the next token after merging
 
             # Merge '>' and '=' into '>='
             if i < len(tokens) and tokens[i].type == TokenType.GREATER_THAN and tokens[i + 1].type == TokenType.EQUAL:
                 tokens[i].value = '>='
                 tokens[i].type = TokenType.GREATER_THAN_OR_EQUAL
-                tokens.pop(i + 1)
+                tokens.pop(i + 1)  # Remove the next token after merging
 
             # Merge '<' and '=' into '<='
             if i < len(tokens) and tokens[i].type == TokenType.LESSER_THAN and tokens[i + 1].type == TokenType.EQUAL:
                 tokens[i].value = '<='
                 tokens[i].type = TokenType.LESSER_THAN_OR_EQUAL
-                tokens.pop(i + 1)
+                tokens.pop(i + 1)  # Remove the next token after merging
 
 
             ## merge **
             if i < len(tokens) and tokens[i].type == TokenType.MUL and tokens[i + 1].type == TokenType.MUL:
                 tokens[i].value = '**'
                 tokens[i].type = TokenType.POWER
-                tokens.pop(i + 1)
+                tokens.pop(i + 1)   # Remove the next token after merging
 
 
 
-        self.tokens=tokens
+        self.tokens=tokens  # Update the tokens list after merging
 
-    ## remove comments
+    # Remove comments from the tokens list
     def removeComments(self):
         tokens = self.tokens
         tokensToPop=[]
+        # Identify tokens that are comments    
         for (i , token) in enumerate(tokens):
             if tokens[i].type == TokenType.COMMENT:
-                tokensToPop.append(i)
+                tokensToPop.append(i)  # Add the index of the comment token
                 
+        # Remove comment tokens            
         for i in tokensToPop:
             tokens.pop(i)
 
-        self.tokens = tokens
+        self.tokens = tokens  # Update the tokens list after removing comments
 
     
-
+    # Identify and classify reserved keywords in the tokens list
     def screenReserved_Keywords(self):
         tokens=self.tokens
+
+        # Check each token and update its type if it is a reserved keyword
         for i in range(len(tokens)):
             if tokens[i].value in RESERVED_KEYWORDS:
                 tokens[i].type=TokenType.RESERVED_KEYWORD
-        self.tokens=tokens
+        self.tokens=tokens  # Update the tokens list after screening for reserved keywords
 
+    # Main method to screen the tokens
     def screen(self):
-        self.mergeTok()
-        self.removeComments()
-        self.screenReserved_Keywords()
-        return self.tokens
+        self.mergeTok()  # Merge specific token patterns
+        self.removeComments()  # Remove comments
+        self.screenReserved_Keywords()  # Screen for reserved keywords
+        return self.tokens  # Return the updated list of tokens
 
