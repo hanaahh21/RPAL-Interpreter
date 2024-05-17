bde2c94823ace7a7cdafad72d7e4d73638a6a98b
Hanaanee Hana <129360538+hanaahh21@users.noreply.github.com>
2024-05-17 17:02:49 +0530

    ASTNode.py

--- rpal-interpreter-master/ASTNode.py
+++ rpal-interpreter-master/ASTNode.py
@@ -2,173 +2,130 @@
 class ASTNode:
 
 
-    def standarize(self, root):
+    def standardize(self, root):
 
         if root == None:
             return None
 
 
-        root.child = self.standarize(root.child)
+        root.child = self.standardize(root.child)
 
-        if root.sibling != None:
-            root.sibling = self.standarize(root.sibling)
+        if root.sib != None:
+            root.sib = self.standardize(root.sib)
 
-        nextSibling = root.sibling;
+        next_sib = root.sib
 
-        # prevSibling = root.previous
-        # nextSibling = root.sibling
+        # prev_sib = root.prev
+        # next_sib = root.sib
 
         match root.type:
             case "let":
-                #print("let")
-                #print( "type : " , root.child.type)
                 if root.child.type == "=":
-                    #print("equal")
                     equal = root.child
-                    P = equal.sibling
+                    P = equal.sib
                     X = equal.child
-                    E = X.sibling
-                    lambdaNode = ASTNode("lambda")
-                    gammaNode = ASTNode("gamma")
-                    gammaNode.child = lambdaNode
-                    lambdaNode.sibling = E
-                    #print("stantdarizing let #######")
-                    X.sibling = P
-                    lambdaNode.child = X
+                    E = X.sib
+                    lambda_Node = ASTNode("lambda")
+                    gamma_Node = ASTNode("gamma")
+                    gamma_Node.child = lambda_Node
+                    lambda_Node.sib = E
+                    X.sib = P
+                    lambda_Node.child = X
+
+                    # P.prev = X
+                    gamma_Node.sib = next_sib
 
 
-
-
-
-
-
-                    # P.previous = X
-                    gammaNode.sibling = nextSibling
-
-
-                    return gammaNode
+                    return gamma_Node
                 else:
-                    root.sibling = nextSibling
+                    root.sib = next_sib
 
                     return root
 
             case "where":
-                if root.child.sibling.type== "=":
+                if root.child.sib.type== "=":
                     P = root.child
-                    equal = P.sibling
+                    equal = P.sib
                     X = equal.child
-                    E = X.sibling
-                    lambdaNode = ASTNode("lambda")
-                    gammaNode = ASTNode("gamma")
+                    E = X.sib
+                    lambda_Node = ASTNode("lambda")
+                    gamma_Node = ASTNode("gamma")
 
-                    gammaNode.child = lambdaNode
-                    lambdaNode.sibling = E
-                    lambdaNode.child = X
+                    gamma_Node.child = lambda_Node
+                    lambda_Node.sib = E
+                    lambda_Node.child = X
 
-                    X.sibling = P
-                    # P.previous = gammaNode
-                    P.sibling = None
+                    X.sib = P
+                    # P.prev = gamma_Node
+                    P.sib = None
 
-                    gammaNode.sibling = nextSibling
+                    gamma_Node.sib = next_sib
 
 
-                    return gammaNode
+                    return gamma_Node
                 else:
-                    root.sibling = nextSibling
+                    root.sib = next_sib
 
                     return root
 
             case "function_form":
                 P = root.child
-                V = P.sibling
-                Vs = V.sibling
+                V = P.sib
+                Vs = V.sib
 
                 newRoot = ASTNode("=")
                 newRoot.child = P
 
-                lambdaNode = ASTNode("lambda")
-                P.sibling = lambdaNode
-                lambdaNode.previous = P
+                lambda_Node = ASTNode("lambda")
+                P.sib = lambda_Node
+                lambda_Node.prev = P
 
-                while Vs.sibling != None:
-                    lambdaNode.child = V
-                    lambdaNode = ASTNode("lambda")
-                    V.sibling = lambdaNode
-                    lambdaNode.previous = V
+                while Vs.sib != None:
+                    lambda_Node.child = V
+                    lambda_Node = ASTNode("lambda")
+                    V.sib = lambda_Node
+                    lambda_Node.prev = V
                     V = Vs
-                    Vs = Vs.sibling
+                    Vs = Vs.sib
 
-                lambdaNode.child = V
-                V.sibling = Vs
-                Vs.previous = V
+                lambda_Node.child = V
+                V.sib = Vs
+                Vs.prev = V
 
-                newRoot.sibling = nextSibling
+                newRoot.sib = next_sib
 
                 return newRoot
 
-            # case "tau":
-            #     E = root.child
-            #     tempE = E
-            #     newRoot = None
-            #     aug = None
-            #     tempESibling = None
-            #
-            #     gamma = ASTNode("gamma")
-            #     gammaL = ASTNode("gamma")
-            #
-            #     gamma.sibling = gammaL
-            #     gammaL.sibling = E
-            #     tempESibling = E.sibling
-            #     E.sibling = None
-            #     aug = ASTNode("aug")
-            #     gammaL.child = aug
-            #
-            #     while E != None:
-            #         gamma = ASTNode("gamma")
-            #         gammaL = ASTNode("gamma")
-            #         aug.sibling = gamma
-            #         gamma.child = gammaL
-            #         gammaL.sibling = E
-            #         tempESibling = E.sibling
-            #         E.sibling = None
-            #         aug = ASTNode("aug")
-            #         gammaL.child = aug
-            #         E = tempESibling
-            #
-            #     aug.sibling = ASTNode('nil')
-            #     tempE.sibling = None
-            #     newRoot.sibling = nextSibling
-            #
-            #     return newRoot
+            
 
             case "within":
-                if root.child.type =="=" and root.child.sibling.type == "=":
+                if root.child.type =="=" and root.child.sib.type == "=":
                     eq1 = root.child
-                    eq2 = eq1.sibling
+                    eq2 = eq1.sib
                     X1 = eq1.child
-                    E1 = X1.sibling
+                    E1 = X1.sib
                     X2 = eq2.child
-                    E2 = X2.sibling
+                    E2 = X2.sib
 
                     newRoot = ASTNode("=")
                     newRoot.child = X2
                     gamma = ASTNode("gamma")
-                    lambdaNode = ASTNode("lambda")
+                    lambda_Node = ASTNode("lambda")
 
-                    X2.sibling = gamma
-                    gamma.previous = X2
-                    gamma.child = lambdaNode
-                    lambdaNode.sibling = E1
-                    E1.previous = lambdaNode
-                    lambdaNode.child = X1
-                    X1.sibling = E2
-                    E2.previous = X1
-                    E1.sibling = None
-                    newRoot.sibling = nextSibling
+                    X2.sib = gamma
+                    gamma.prev = X2
+                    gamma.child = lambda_Node
+                    lambda_Node.sib = E1
+                    E1.prev = lambda_Node
+                    lambda_Node.child = X1
+                    X1.sib = E2
+                    E2.prev = X1
+                    E1.sib = None
+                    newRoot.sib = next_sib
 
                     return newRoot
                 else :
-                    root.sibling = nextSibling
+                    root.sib = next_sib
 
                     return root
 
@@ -180,27 +137,27 @@ class ASTNode:
                 tau = ASTNode("tau")
 
                 newRoot.child = comma
-                comma.sibling = tau
-                tau.previous = comma
+                comma.sib = tau
+                tau.prev = comma
 
                 X = eq.child
-                E = X.sibling
+                E = X.sib
 
                 comma.child = X
                 tau.child = E
 
-                eq = eq.sibling
+                eq = eq.sib
                 while eq != None:
-                    X.sibling = eq.child
-                    eq.child.previous = X
-                    E.sibling = eq.child.sibling
-                    eq = eq.sibling
-                    X = X.sibling
-                    E = E.sibling
+                    X.sib = eq.child
+                    eq.child.prev = X
+                    E.sib = eq.child.sib
+                    eq = eq.sib
+                    X = X.sib
+                    E = E.sib
 
-                X.sibling = None
-                E.sibling = None
-                newRoot.sibling = nextSibling
+                X.sib = None
+                E.sib = None
+                newRoot.sib = next_sib
 
 
                 return newRoot
@@ -208,125 +165,113 @@ class ASTNode:
             case "rec":
                 eq = root.child
                 X = eq.child
-                E = X.sibling
+                E = X.sib
 
                 new_root = ASTNode("=")
                 new_root.child = X
 
-                copy_X = X.createCopy()
+                X_copy = X.createCopy()
                 gamma = ASTNode("gamma")
-                X.sibling = gamma
-                gamma.previous = X
+                X.sib = gamma
+                gamma.prev = X
 
-                # TODO: may need to remove <> later !!!!
-                y_star = ASTNode("Y*")
-                gamma.child = y_star
+                
+                yStar = ASTNode("Y*")
+                gamma.child = yStar
                 lambda_ = ASTNode("lambda")
-                y_star.sibling = lambda_
-                lambda_.previous = y_star
+                yStar.sib = lambda_
+                lambda_.prev = yStar
 
-                lambda_.child = copy_X
-                copy_X.sibling = E
-                E.previous = copy_X
-                new_root.sibling = nextSibling
+                lambda_.child = X_copy
+                X_copy.sib = E
+                E.prev = X_copy
+                new_root.sib = next_sib
 
 
                 return new_root
 
             case "@":
                 E1 = root.child
-                N = E1.sibling
-                E2 = N.sibling
+                N = E1.sib
+                E2 = N.sib
 
                 new_root = ASTNode("gamma")
                 gamma_l = ASTNode("gamma")
 
                 new_root.child = gamma_l
-                gamma_l.sibling = E2
-                # E2.previous = gamma_l
+                gamma_l.sib = E2
+                # E2.prev = gamma_l
                 gamma_l.child = N
-                N.sibling = E1
-                # E1.previous = N
-                E1.sibling = None
-                new_root.sibling=nextSibling
+                N.sib = E1
+                # E1.prev = N
+                E1.sib = None
+                new_root.sib=next_sib
 
                 return new_root
 
             case _:
                 return root
 
-        # #print("root" , root.type)
-        # root.previous = prevSibling
-        # root.sibling = nextSibling
         return root
 
     def __init__(self, type):
         self.type = type
-        self.value = None
-        self.sourceLineNumber = -1
+        self.val = None
+        self.source_LineNum = -1
         self.child = None
-        self.sibling = None
-        self.previous = None
+        self.sib = None      #sibling
+        self.prev = None     #previous
         self.indentation = 0
 
-    def print_tree(self):
-        # #print(self.type)
+    def Tree(self):
 
         if self.child:
-            # #print(" child of " + str(self.type) + " is ",end=" ")
-            self.child.print_tree()
-        if self.sibling:
-            # #print(" sibling of " + str(self.type) + " is " ,end=" ")
+            self.child.Tree()
+        if self.sib:
+            self.sib.Tree()
 
-            self.sibling.print_tree()
-
-    def print_tree_to_cmd(self):
+    def Tree_to_cmd(self):
 
         for i in range(self.indentation):
             print(".", end="")
-        if self.value is not None:
-            print("<"+str(self.type.split(".")[1]) +":" + str(self.value)+">")
+        if self.val is not None:
+            print("<"+str(self.type.split(".")[1]) +":" + str(self.val)+">")
         else:print(str(self.type))
-        # print(self.type, end=" ")
-        # if self.child:
-        #     #print("child of " + str(self.type) + " is ", self.child.type)
-        # if self.sibling:
-        #     #print("sibling of " + str(self.type) + " is ", self.sibling.type)
-        #
+        
 
         if self.child:
             self.child.indentation = self.indentation + 1
-            self.child.print_tree_to_cmd()
-        if self.sibling:
-            self.sibling.indentation = self.indentation
-            self.sibling.print_tree_to_cmd()
+            self.child.Tree_to_cmd()
+        if self.sib:
+            self.sib.indentation = self.indentation
+            self.sib.Tree_to_cmd()
 
     # output to the file
-    def print_tree_to_file(self, file):
+    def Tree_to_file(self, file):
 
         for i in range(self.indentation):
             file.write(".")
         # if(self.type ==)
-        if self.value is not None:
+        if self.val is not None:
 
-            file.write("<"+str(self.type.split(".")[1])+":"+str(self.value)+">" + "\n")
+            file.write("<"+str(self.type.split(".")[1])+":"+str(self.val)+">" + "\n")
         else :
             file.write(str(self.type) + "\n")
 
         if self.child:
 
             self.child.indentation = self.indentation + 1
-            self.child.print_tree_to_file(file)
-        if self.sibling:
-            self.sibling.indentation = self.indentation
-            self.sibling.print_tree_to_file(file)
+            self.child.Tree_to_file(file)
+        if self.sib:
+            self.sib.indentation = self.indentation
+            self.sib.Tree_to_file(file)
 
     def createCopy (self):
         node = ASTNode(self.type)
-        node.value = self.value
-        node.sourceLineNumber = self.sourceLineNumber
+        node.val = self.val
+        node.source_LineNum = self.source_LineNum
         node.child = self.child
-        node.sibling = self.sibling
-        node.previous = self.previous
+        node.sib = self.sib
+        node.prev = self.prev
         return node
 
