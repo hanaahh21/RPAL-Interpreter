82845d78de3f12c8c4389d388433064a58845a39
hananazir-2001 <129369824+hananazir-2001@users.noreply.github.com>
2024-05-17 18:55:43 +0530

    Update test.py

--- rpal-interpreter-master/test.py
+++ rpal-interpreter-master/test.py
@@ -1,10 +1,15 @@
+# Import the subprocess module to run external scripts
 import subprocess
+# Import the os module to interact with the operating system
 import os
 # Arguments to be passed to the called script
 arg1 = "test_cases/test"
+
+# Get a list of all files in the test_cases directory
 dir=os.listdir("test_cases")
 for file in dir :
     # print("output for file: ", file ,end=" ")
+    # Run the external script 'myrpal.py' with the file as an argument
     subprocess.run(['python', 'myrpal.py', "test_cases\\"+ file])
 
 
