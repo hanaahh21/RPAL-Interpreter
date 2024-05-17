103a8597736cebce24560537cc26011b8d693d8b
Hanaanee Hana <129360538+hanaahh21@users.noreply.github.com>
2024-05-17 17:04:00 +0530

    Environment.py

--- rpal-interpreter-master/Envronment.py
+++ rpal-interpreter-master/Envronment.py
@@ -3,7 +3,7 @@ import logging
 from ASTNode import ASTNode
 
 
-class Envronment:
+class Environment:
     logger = logging.getLogger(__name__)
 
     def __init__(self, index):
@@ -11,29 +11,29 @@ class Envronment:
         self.map_vars = {}
         self.parent = None
 
-    def set_env_params(self, parent_env, key, value):
-        self.map_vars[key] = value
-        if isinstance(key, ASTNode) and isinstance(value, ASTNode):
+    def set_env_parameters(self, parent_env, key, val):
+        self.map_vars[key] = val
+        if isinstance(key, ASTNode) and isinstance(val, ASTNode):
             pass
-            # print("setEnvParams: {} | {} | value: {}".format(key, key.name, value.name))
+            # print("setEnvParams: {} | {} | val: {}".format(key, key.name, val.name))
         else:
-            # self.logger.info("setEnvParams: key: {} | value: {}".format(key, value.name))
+            # self.logger.info("setEnvParams: key: {} | val: {}".format(key, val.name))
             pass
         self.parent = parent_env
         # print(self.map_vars)
         # print("setting parent of environment: {} as env: {}".format(self.idx, parent_env.idx))
 
-    def get_env_idx(self):
+    def get_env_index(self):
         return self.idx
 
     def get_val(self, key):
         # self.logger.info("getVal: {} | {}".format(key, key.name))
         if key in self.map_vars.keys():
-            value = self.map_vars[key]
+            val = self.map_vars[key]
             self.logger.info("found in cur env id {}".format(self.idx))
-            if isinstance(value, ASTNode):
-                self.logger.info("value: {}".format(value.value))
-            return value
+            if isinstance(val, ASTNode):
+                self.logger.info("val: {}".format(val.val))
+            return val
         else:
             self.logger.info("not found in cur env")
             return None
